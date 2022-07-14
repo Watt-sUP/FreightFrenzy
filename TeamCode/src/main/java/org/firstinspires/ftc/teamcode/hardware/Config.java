ecbed3e2d38286c74b0008cf62a675efb397769d
Powercube7 <80365192+Powercube7@users.noreply.github.com>
2022-07-14 23:01:40 +0300

    Update Config.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Config.java
@@ -14,6 +14,6 @@ public class Config {
     public static final String clap = "clap";
     public static final String ruleta = "rul";
 
-    public static final String vuforiaKey = "AT4pxIn/////AAAAGSL31MQ2OkEAlE8fAGFaXLprsCGiwn5e81ZDhALBFUMK45pVGTrKvkFV9ZBC8LGo2fubVHcyc2JBpk2bsXVf/0zESirRkEkFjIItegiTFBB6wwQeeBcANTIZAFH1EjAo6QDGxlMTyhJ6JJQmrm2yBPFJFpfWDus1E34IVIyLHc8V/iCSTuegaorAlk1MXV7r2jog5G5rwsWhfD6CDx2E85s1cD20eEC4XQqx2pgcbG0CB1ohiPIYG0jtj373VY8gn9PfX2YJDBe/sLAx4IbxQxvtpgL5PAhAFcTdPfYAi6GmUzLbWQDITzao3dFlxiJjRA8fklV5KsBKJFj6viP+m3HybrTgW6kR+VOFbU2J2wD8";
+    public static final String vuforiaKey = null;
 
 }
